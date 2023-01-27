a663ef696b996210a087cbccb7986b6eb5c25902
rithyskun <rithy.skun@outlook.com>
2023-01-27 10:36:50 +0700

    removed more lib that not use

--- server/api/blog/blog.post.ts
+++ server/api/blog/blog.post.ts
@@ -1,22 +1,8 @@
-import fs from 'fs'
-import Mustache from 'mustache'
 import { createBlog } from '~~/modules/blog/blog.service'
 
 export default defineEventHandler(async (event) => {
     try {
         const body = await readBody(event)
-        let template = fs.readFileSync('./template/blogTemplate.md').toString()
-
-        let bodyData = {
-            title: body.title,
-            coverImageUrl: "",
-            tags: body.tags,
-            blogBody: body.blogBody
-        }
-        let output = Mustache.render(template, bodyData)
-        if (output) {
-            fs.writeFileSync(`./content/3.blogs/${body.title}.md`, output)
-        }
         const blog = await createBlog(body);
         return blog
     } catch (error: any) {
