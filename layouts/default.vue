bd9ed5048a880fce358189ea97b4323c8a41294e
rithyskun <rithy.skun@outlook.com>
2023-07-17 20:19:12 +0700

    fix: remove the unuse lib

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,25 +5,16 @@ useHead({
     {
       name: 'description',
       content:
-        'A new developer from Cambodia, I love coding and explore the new tech.',
-    },
+        'A new developer from Cambodia, I love coding and explore the new tech.'
+    }
   ],
   bodyAttrs: {
-    class: '',
+    class: ''
   },
   script: [
-<<<<<<< HEAD
-    { children: 'console.log(\'Welcome to my blog\')' },
-    { src: 'https://www.googletagmanager.com/gtag/js?id=G-0RYW18N27X' },
-=======
     { children: "console.log('Welcome to my blog')" },
->>>>>>> parent of 3263945 (changed the ga to use plugin)
-    {
-      'src': 'https://plausible.io/js/script.js',
-      'defer': true,
-      'data-domain': 'rithyskun-blogs.netlify.app',
-    },
-  ],
+    { src: 'https://www.googletagmanager.com/gtag/js?id=G-0RYW18N27X' }
+  ]
 })
 </script>
 
